c2fe5263a702005ebf87ef631ac5349a2664c86c
Christian Bär <teggno@hotmail.com>
2019-01-03 00:24:22 +0100

    Escape the url when storing to PartitionKey

--- Track/index.js
+++ Track/index.js
@@ -1,22 +1,33 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    
-    if (req.body && req.body.url && req.body.fingerprint) {
-        context.bindings.requests = [{
-            PartitionKey: req.body.url,
-            RowKey: context.bindingData.sys.randGuid,
-            fingerprint: req.body.fingerprint,
-            navigator: JSON.stringify(req.body.navigator)
-        }];
+module.exports = async function(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-        context.res = {
-            status: 200
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "POST request expected with a body that is a JSON object containing an url and a fingerprint field."
-        };
-    }
+  if (req.body && req.body.url && req.body.fingerprint) {
+    context.bindings.requests = [
+      {
+        PartitionKey: escapeUrl(req.body.url),
+        RowKey: context.bindingData.sys.randGuid,
+        fingerprint: req.body.fingerprint,
+        navigator: JSON.stringify(req.body.navigator)
+      }
+    ];
+
+    context.res = {
+      status: 200
+    };
+  } else {
+    context.res = {
+      status: 400,
+      body:
+        "POST request expected with a body that is a JSON object containing an url and a fingerprint field."
+    };
+  }
 };
+
+function escapeUrl(url) {
+  return url
+    .replace(/\@/g, "@@")
+    .replace(/\//g, "@s")
+    .replace(/\#/g, "@h")
+    .replace(/\\/g, "@b")
+    .replace(/\?/g, "@q");
+}
