401acc010d01ace8ec7eac5463abce987b18bbbe
Christian Bär <teggno@hotmail.com>
2019-01-03 10:00:23 +0100

    Changed record structure.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Serverless web analytics
-Serverless web analytics running on azure tracking any kind of (low traffic) web page.
+Serverless web analytics running on azure tracking any kind of web page. 
+
+Page hits are stored in azure table storage.
+
+
+## Analysis UI
+* Total of all pages over time (time on x axis)
+* Last 24 hours by page
+* Measures to look at: sessions, page views, visitors
+
+
+## Notifications
+Get notified when traffic is increasing siginificantly. Goal is to not to have
+to go and check proactively.
--- Track/index.js
+++ Track/index.js
@@ -1,33 +1,42 @@
 module.exports = async function(context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  if (req.body && req.body.url && req.body.fingerprint) {
-    context.bindings.requests = [
-      {
-        PartitionKey: escapeUrl(req.body.url),
-        RowKey: context.bindingData.sys.randGuid,
-        fingerprint: req.body.fingerprint,
-        navigator: JSON.stringify(req.body.navigator)
-      }
-    ];
-
-    context.res = {
-      status: 200
-    };
-  } else {
+  const { isValid, messages } = validate(req.body);
+  if (!isValid) {
     context.res = {
       status: 400,
-      body:
-        "POST request expected with a body that is a JSON object containing an url and a fingerprint field."
+      body: messages.join(", ")
     };
+    return;
   }
+
+  const record = Object.assign(
+    {},
+    {
+      PartitionKey: req.body.timestampUtc,
+      RowKey: context.bindingData.sys.randGuid,
+      url: req.body.url,
+      fingerprint: req.body.fingerprint,
+      referrer: req.body.referrer
+    },
+    req.body.navigator
+  );
+  context.bindings.hits = [record];
+
+  context.res = {
+    status: 200
+  };
 };
 
-function escapeUrl(url) {
-  return url
-    .replace(/\@/g, "@@")
-    .replace(/\//g, "@s")
-    .replace(/\#/g, "@h")
-    .replace(/\\/g, "@b")
-    .replace(/\?/g, "@q");
+function validate(potentialHit) {
+  const messages = [];
+
+  if (!potentialHit.url) messages.push("url field expected");
+  if (!potentialHit.timestampUtc) messages.push("timestampUtc field expected");
+  if (!potentialHit.fingerprint) messages.push("fingerprint field expected");
+
+  return {
+    isValid: messages.length === 0,
+    messages: messages
+  };
 }
