7c2af3239963fbbaf136dd01809f3f75b0f0bbba
Christian Bär <teggno@hotmail.com>
2019-01-03 12:40:19 +0100

    Fix structure of response in PageViews function

--- PageViews/index.js
+++ PageViews/index.js
@@ -4,13 +4,16 @@ module.exports = async function(context, req) {
   // query string parameters start end end are expected and used in the table
   // binding
   const groupByUrl = R.groupBy(h => h.url);
-  context.res = R.sort(
-    R.descend(i => i.pageViews),
-    Object.values(
-      R.map(
-        g => ({ url: g[0].url, pageViews: g.length }),
-        groupByUrl(context.bindings.hits)
+  context.res = {
+    status: 200,
+    body: R.sort(
+      R.descend(i => i.pageViews),
+      Object.values(
+        R.map(
+          g => ({ url: g[0].url, pageViews: g.length }),
+          groupByUrl(context.bindings.hits)
+        )
       )
     )
-  );
+  };
 };
